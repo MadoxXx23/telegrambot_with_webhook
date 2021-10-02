3cde6697499b418538332a634740ed434a0bd7fe
Belous <madoxxx24@gmail.com>
2021-10-02 17:33:53 +0300

    exchan

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ WEBHOOK_PATH = ''
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = 'localhost'  # or ip
-WEBAPP_PORT = 3001
+# WEBAPP_HOST = 'localhost'  # or ip
+# WEBAPP_PORT = 3001
 
 
 bot = Bot(token=API_TOKEN)
